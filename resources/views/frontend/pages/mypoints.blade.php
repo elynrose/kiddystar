6287d74baf4d4f62b450e1c7026a01b1be320dae
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-05-02 22:13:29 -0400

    add name to mypoints

--- resources/views/frontend/pages/mypoints.blade.php
+++ resources/views/frontend/pages/mypoints.blade.php
@@ -6,7 +6,7 @@
 
             <div class="card">
                 <div class="card-header">
-                <h5></h5>
+                <h5>{{$name}}</h5>
                 </div>
 
                 <div class="card-body">
