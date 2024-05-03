a5fa25216b61402a43be1b077e31297af41078e8
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-05-02 21:35:42 -0400

    scan - change auth to guest

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -18,7 +18,7 @@ class ScanController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('guest');
     }
 
     /**
