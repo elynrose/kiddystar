8bf2e7865b8c36832e4d2c77964cf7eab83cc0f9
eliezer.ayertey@lcaforesthills.org <eliezer.ayertey@lcaforesthills.org>
2024-05-02 22:02:02 -0400

    fixed error for auth check when card exists

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -26,12 +26,13 @@ class ScanController extends Controller
         $card = $this->cardExist($request->card);
         
         if ($card) {
-
             if(Auth::check()){
             // Check if the card is already registered to a user.
             $userCard = UserCard::where('card_id', $card->id)->first();
 
             if ($userCard) {
+
+                if (Auth::check()){
                 $settings2 = [
                     'chart_title'           => 'Points',
                     'chart_type'            => 'line',
@@ -69,18 +70,16 @@ class ScanController extends Controller
 
                 // Return the view with the collected data.
                 return view('frontend.pages.scan', compact('userCard', 'user_points', 'all_points', 'user_claims', 'all_claims', 'chart2'));
-            }
-            else
-            {
-                //If the user is not logged in but card exists pull the points for the particular card
-                $user_points = Point::where('card_id', $userCard->card_id)
-                                    ->sum('points');
-                $user_claims = Claim::where('card_id', $userCard->card_id)
-                                    ->sum('points');
-                $total_points_earned = ($user_points - $user_claims);
+            } else {
+                  //If the user is not logged in but card exists pull the points for the particular card
+                  $user_points = Point::where('card_id', $userCard->card_id)
+                  ->sum('points');
+$user_claims = Claim::where('card_id', $userCard->card_id)
+                  ->sum('points');
+$total_points_earned = ($user_points - $user_claims);
 
-               // return view('frontend.pages.mypoints', compact('total_points_earned'));  
-                
+ return view('frontend.pages.mypoints', compact('total_points_earned'));  
+            }
             }
             } else {
                 //Check if the user is logged in
